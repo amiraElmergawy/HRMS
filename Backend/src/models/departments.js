c31008c3f2ed46bf012a19f6b4bb9c37ec75eb9f
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2021-02-01 12:02:22 +0200

    Removed depricated pre-remove function

--- Backend/src/models/departments.js
+++ Backend/src/models/departments.js
@@ -26,22 +26,6 @@ departmentSchema.virtual('employees',{
     foreignField:'jobData.dept'
 })
 
-departmentSchema.pre('remove', async function(next){
-    console.log('hhhhhhh')
-    // const dept= this
-    // await employeeModel.updateMany(
-    //     {'jobData.dept': dept._id},
-    //     { $set: { 'jobData.dept' : new ObjectID(1)}}
-    // )
-    // throw new Error('inside preeeeeeeee')
-//    console.log(dept)
-    // await dept.populate('employees').execPopulate()
-    // console.log(dept.employees)
-    // if (dept.employees) {
-    //     throw new customValidator.ValidationError('لا يسمح بمسح هذا القسم لوجود عاملين به')
-    // }
-    // // else next()
-})
 
 
 const departmentModel = mongoose.model('department', departmentSchema)
