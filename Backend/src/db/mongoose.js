13cc21a0a3c895df880c033237cbeea2a8a7b0c4
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2021-02-16 13:58:58 +0200

    Update mongoose.js

--- Backend/src/db/mongoose.js
+++ Backend/src/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/HRMS',{
     useNewUrlParser: true,
     useCreateIndex:true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology:true
 })
